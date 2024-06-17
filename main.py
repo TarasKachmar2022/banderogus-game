4c08149475340d277db08b5661707c6100cfabed
Taras <kachmar.taras@gmail.com>
2024-06-17 13:15:04 +0300

    add player controls

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_RIGHT, K_LEFT
 
 pygame.init()
 
@@ -17,12 +19,17 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+# player_speed = [1, 1]
+
+player_move_top = [0, -1]
+player_move_right = [1, 0]
+player_move_down = [0, 1]
+player_move_left = [-1, 0]
 
 playing = True
 
 while playing:
-        FPS.tick(120)
+        FPS.tick(660)
 
         for event in pygame.event.get():
                 if event.type == QUIT:
@@ -30,20 +37,35 @@ while playing:
 
         main_display.fill(COLOR_BLACK)
 
-        if player_rect.top == 0:
-                player_speed = [-1, 1]
+        keys = pygame.key.get_pressed()
 
-        if player_rect.right >= WIDTH:
-                player_speed[0] = -player_speed[0]
+        if keys[K_UP] and player_rect.top > 0:
+                player_rect = player_rect.move(player_move_top)
 
-        if player_rect.bottom >= HEIGHT:
-                player_speed[1] = -player_speed[1]
+        if keys[K_RIGHT] and player_rect.right < WIDTH:
+                player_rect = player_rect.move(player_move_right)
 
-        if player_rect.left == 0:
-                player_speed = [1, 1]
+        if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+                player_rect = player_rect.move(player_move_down)
+                
+        if keys[K_LEFT] and player_rect.left > 0:
+                player_rect = player_rect.move(player_move_left)
+
+# Рандомна зміна напрямку
+        # if player_rect.top <= 0:
+        #         player_speed = random.choice(([-1, 1], [1, 1]))
+
+        # if player_rect.right >= WIDTH:
+        #         player_speed = random.choice(([-1, -1], [-1, 1]))
+
+        # if player_rect.bottom >= HEIGHT:
+        #         player_speed = random.choice(([1, -1], [-1, -1]))
+
+        # if player_rect.left <= 0:
+        #         player_speed = random.choice(([1, 1], [1, -1]))
 
         main_display.blit(player, player_rect)
 
-        player_rect = player_rect.move(player_speed)
+        # player_rect = player_rect.move(player_speed)
 
         pygame.display.flip()
